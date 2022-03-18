ee72ef761e92e8da9ccd86eb59e2e67792df2531
pedrobrnpng <66803480+pedrobrnpng@users.noreply.github.com>
2022-03-18 14:10:06 +0000

    removed unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,5 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import Navbar from '../components/navbar';
 import LandingScreen from '../page-components/LandingScreen';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return <LandingScreen />;
