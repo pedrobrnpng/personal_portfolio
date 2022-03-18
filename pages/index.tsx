c680179d0c713f268f5f3ecf79884bfb4dd4d5cc
pedrobrnpng <66803480+pedrobrnpng@users.noreply.github.com>
2022-03-18 17:31:57 +0000

    added about me to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from 'next';
+import AboutMe from '../page-components/AboutMe';
 import LandingScreen from '../page-components/LandingScreen';
 
 const Home: NextPage = () => {
-  return <LandingScreen />;
+  return (
+    <div>
+      <LandingScreen />
+      <AboutMe />
+    </div>
+  );
 };
 
 export default Home;
