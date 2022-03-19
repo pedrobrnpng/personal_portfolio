dfb01fe2dbe60c11de185c6480db636b34674105
pedrobrnpng <66803480+pedrobrnpng@users.noreply.github.com>
2022-03-19 16:35:06 +0000

    added built project section to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import AboutMe from '../page-components/AboutMe';
+import BuiltProjectsSection from '../page-components/BuiltProjectsSection';
 import LandingScreen from '../page-components/LandingScreen';
 
 const Home: NextPage = () => {
   return (
     <div>
+      <Head>
+        <title>Pedro Brandão | Personal Portfolio</title>
+      </Head>
       <LandingScreen />
       <AboutMe />
+      <BuiltProjectsSection />
     </div>
   );
 };
