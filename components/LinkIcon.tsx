69fcc6a6ee07b645d7314cf598e7d594e9455d67
pedrobrnpng <66803480+pedrobrnpng@users.noreply.github.com>
2022-03-19 16:34:28 +0000

    Icon component

---
+++ components/LinkIcon.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+interface Props {
+  link: string;
+  Icon: React.ReactNode;
+}
+
+export const LinkIcon: React.FC<Props> = (props) => {
+  const { Icon, link } = props;
+  return (
+    <div className="pr-3 text-3xl transition-all hover:cursor-pointer hover:text-brown-beaver">
+      <Link href={link}>{Icon}</Link>
+    </div>
+  );
+};
