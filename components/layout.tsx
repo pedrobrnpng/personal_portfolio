ba2c8c42b05632eeeea54906a9aeed138a00d2a7
pedrobrnpng <66803480+pedrobrnpng@users.noreply.github.com>
2022-03-19 17:59:04 +0000

    changed layout 2 hav scroll out effect on contacts

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,3 +1,7 @@
-export default function Layout({ children }: any) {
-  return <div className="m-auto px-6 py-4 lg:max-w-screen-lg">{children}</div>;
+export default function Layout({ children }: any, id: string) {
+  return (
+    <div id={id} className="relative z-20 bg-dark-purple shadow-2xl">
+      <div className="m-auto px-6 py-4 lg:max-w-screen-lg">{children}</div>
+    </div>
+  );
 }
