7f4606c4680fae199cd317a4815b26f73395f145
pedrobrnpng <66803480+pedrobrnpng@users.noreply.github.com>
2022-03-19 16:32:31 +0000

    added alternative text

--- components/illustration.tsx
+++ components/illustration.tsx
@@ -1,5 +1,12 @@
 import Image from 'next/image';
 
 export const Illustration = () => {
-  return <Image src="/Frame.svg" width={500} height={400} />;
+  return (
+    <Image
+      src="/Frame.svg"
+      width={500}
+      height={400}
+      alt="Portrait Illustration"
+    />
+  );
 };
