3a8a31ce512ed6a796f56d24588acb5e2247ef53
pedrobrnpng <66803480+pedrobrnpng@users.noreply.github.com>
2022-03-23 16:29:53 +0000

    animated hamburger menu

--- components/hamburger.tsx
+++ components/hamburger.tsx
@@ -1,19 +1,27 @@
-export const Hamburger = () => {
+export const Hamburger = (isOpen: boolean) => {
   return (
-    <div className="absolute block w-5 -translate-x-1/2 -translate-y-1/2 transform">
+    <div className="relative h-10 w-10 bg-black focus:outline-none">
       <span className="sr-only">Open main menu</span>
-      <span
-        aria-hidden="true"
-        className="absolute top-0 block h-0.5 w-5 transform bg-current transition duration-500 ease-in-out"
-      ></span>
-      <span
-        aria-hidden="true"
-        className="absolute top-2 block h-0.5 w-5 transform   bg-current transition duration-500 ease-in-out"
-      ></span>
-      <span
-        aria-hidden="true"
-        className="absolute top-4 block h-0.5 w-5 transform bg-current  transition duration-500 ease-in-out"
-      ></span>
+      <div className="absolute left-1/2 top-1/2 block w-5 -translate-x-1/2 -translate-y-1/2 transform">
+        <span
+          aria-hidden="true"
+          className={`
+        ${isOpen ? 'rotate-45' : 'translate-y-0 rotate-0'}
+        absolute top-0 block h-0.5 w-5 transform bg-current transition duration-500 ease-in-out`}
+        />
+        <span
+          aria-hidden="true"
+          className={`
+        ${isOpen && 'opacity-0'}
+        absolute top-2 block h-0.5 w-5 transform  bg-current transition duration-500 ease-in-out`}
+        />
+        <span
+          aria-hidden="true"
+          className={`
+        ${isOpen ? '-rotate-45' : 'translate-y-0 rotate-0'}
+        absolute top-4 block h-0.5 w-5 transform bg-current transition duration-500 ease-in-out`}
+        />
+      </div>
     </div>
   );
 };
