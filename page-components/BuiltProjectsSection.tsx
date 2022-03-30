ff506857800ea7d4821fd129bdb03d3b25839c60
pedrobrnpng <66803480+pedrobrnpng@users.noreply.github.com>
2022-03-30 19:30:51 +0100

    new descriptions

--- page-components/BuiltProjectsSection.tsx
+++ page-components/BuiltProjectsSection.tsx
@@ -9,9 +9,8 @@ export default function BuiltProjectsSection() {
       type: 'Featured Project',
       title: 'AASM Furniture',
       description:
-        'A Web app for the company’silable. It has authentication to limit the visibility to only people that work with the company, in order to prevent copying of ideas. Also works as a way to get to know the company better.',
+        "A Web app for the company's clients be able to see their products. It has authentication to limit the visibility to only people that work with the company, in order to prevent copying of ideas. Also works as a way to get to know the company better.",
       tech: ['Next.js', 'Prismic', 'NextAuth', 'Vercel', 'MongoDB'],
-      github: 'https://www.github.com/pedrobrnpng/aasm-moveis',
       live: 'https://www.aasm-furniture.com',
       pic: '/aasm_print.jpg',
     },
@@ -19,10 +18,10 @@ export default function BuiltProjectsSection() {
       type: 'Featured Project',
       title: "Inej's Portfolio",
       description:
-        'Lörem ipsum antinusat ongar ifall decidäbelt. Astrologi doktig fir tesade. Kösk stenohyressade operasion. Bere delingar dolig trerade måling. Tetick saliga, termotism, res. Begt teleles pret. Del onera krolig. Trende fase homore.',
+        "A digital portfolio to showcase Inês's works. Built with Next.js for it's quick way to set up new pages, and uses Prismic as it's CMS to allow the client to add new data to the page.",
       tech: ['Next.js', 'Prismic', 'Vercel'],
-      github: 'https://www.github.com/pedrobrnpng/inej-portfolio',
-      live: 'https://www.inespinheiro.vercel.app',
+      github: 'https://github.com/pedrobrnpng/inej_portfolio',
+      live: 'https://inespinheiro.vercel.app',
       pic: '/inej_print.jpg',
     },
   ];
